6a7545f7803914c3357244eb28d054c7579a777e
Zekarias Araya <zekarias99@gmail.com>
2013-01-14 07:02:16 -0800

    Setup gem dependencies and run rspec generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'b61633f7c142351cf74892e4d7e80c430428c4132097b663d9347e7fc612a392c4d70e499219c2024a1dfe71e3755b78d65b6e16bb09e73b4232c2749d9ad68c'
