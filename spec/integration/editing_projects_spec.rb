a3aa3c7955b547394afdce1da2e28d28668c43c8
Zekarias Araya <zekarias99@gmail.com>
2013-02-04 21:47:52 -0800

    Add updating projects functionality

---
+++ spec/integration/editing_projects_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature "Editing Projects" do 
+  before do
+    Factory(:project, :name => "Sublime Text 2")
+    visit '/'
+    click_link "Sublime Text 2"
+    click_link "Edit Project"   
+  end
+
+  scenario "Updating a project" do
+    fill_in "Name", with: "Sublime Text 2"
+    click_button "Update Project"
+    page.should have_content("Project has been updated.")
+  end
+  scenario "Updating a project with invalid attributes is bad" do
+    fill_in "Name", :with => ""
+    click_button "Update Project"
+    page.should have_content("Project has not been updated.")
+  end
+end
