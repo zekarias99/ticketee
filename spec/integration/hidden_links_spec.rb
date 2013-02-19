bbe3c3bce03b3c0725996e1df145c9f610486407
Zekarias Araya <zekarias99@gmail.com>
2013-02-19 08:41:58 -0800

    Lock down specific project controller actions for admins only

--- spec/integration/hidden_links_spec.rb
+++ spec/integration/hidden_links_spec.rb
@@ -3,11 +3,22 @@ require 'spec_helper'
 feature "hidden links" do
 	let(:user) { Factory(:confirmed_user) }
 	let(:admin) { Factory(:admin_user) }
+	let(:project) { Factory(:project) }
 
 	context "annanimous users" do
 		scenario "cannot see the New Project link" do
 			visit '/'
 			assert_no_link_for "New Project"
+		end
+
+		scenario "cannot see the Edit Project link" do
+			visit project_path(project)
+			assert_no_link_for "Edit Project"
+		end
+
+		scenario "cannot see the Delete Project link" do
+			visit project_path(project)
+			assert_no_link_for "Delete Project"
 		end
 	end
 
@@ -17,6 +28,16 @@ feature "hidden links" do
 			visit '/'
 			assert_no_link_for "New Project"
 		end
+
+		scenario "cannot see the Edit Project link" do
+			visit project_path(project)
+			assert_no_link_for "Edit Project"
+		end
+
+		scenario "cannot see the Delete Project link" do
+			visit project_path(project)
+			assert_no_link_for "Delete Project"
+		end		
 	end
 
 	context "admin users" do
@@ -25,6 +46,16 @@ feature "hidden links" do
 			visit '/'
 			assert_link_for "New Project"
 		end
+
+		scenario "can see the Edit Project link" do
+			visit project_path(project)
+			assert_link_for "Edit Project"
+		end
+
+		scenario "can see the Delete Project link" do
+			visit project_path(project)
+			assert_link_for "Delete Project"
+		end		
 	end
 end
 
