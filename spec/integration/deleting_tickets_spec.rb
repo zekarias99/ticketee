2fdc14963e98e5d992963b2c9520f9332e544d5b
Zekarias Araya <zekarias99@gmail.com>
2013-02-27 14:22:02 -0800

    Restrict destroy action to only pople with permishion

--- spec/integration/deleting_tickets_spec.rb
+++ spec/integration/deleting_tickets_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "Editing tickets" do
+feature 'Deleting tickets' do
   let!(:project) { Factory(:project) }
   let!(:user) { Factory(:confirmed_user) }
   let!(:ticket) do
@@ -11,27 +11,16 @@ feature "Editing tickets" do
   
   before do
     define_permission!(user, "view", project)
-    define_permission!(user, "edit tickets", project)
+    define_permission!(user, "delete tickets", project)
     sign_in_as!(user)
     visit '/'
     click_link project.name
     click_link ticket.title
-    click_link "Edit Ticket"
   end
   
-  scenario "Updating a ticket" do
-    fill_in "Title", :with => "Make it really shiny!"
-    click_button "Update Ticket"
-    page.should have_content "Ticket has been updated."
-    within("#ticket h2") do
-      page.should have_content("Make it really shiny!")
-    end
-    page.should_not have_content ticket.title
-  end
-  
-  scenario "Updating a ticket with invalid information" do
-    fill_in "Title", :with => ""
-    click_button "Update Ticket"
-    page.should have_content("Ticket has not been updated.")
+  scenario "Deleting a ticket" do
+    click_link "Delete Ticket"
+    page.should have_content("Ticket has been deleted.")
+    page.current_url.should == project_url(project)
   end
 end
