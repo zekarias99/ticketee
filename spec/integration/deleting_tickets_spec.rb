4e703745b87e3d9219db679fcd08ce789c58824c
Zekarias Araya <zekarias99@gmail.com>
2013-02-27 14:05:03 -0800

    Restrict ticket updating to only those who have permision

--- spec/integration/deleting_tickets_spec.rb
+++ spec/integration/deleting_tickets_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'Deleting tickets' do
+feature "Editing tickets" do
   let!(:project) { Factory(:project) }
   let!(:user) { Factory(:confirmed_user) }
   let!(:ticket) do
@@ -11,15 +11,27 @@ feature 'Deleting tickets' do
   
   before do
     define_permission!(user, "view", project)
+    define_permission!(user, "edit tickets", project)
     sign_in_as!(user)
     visit '/'
     click_link project.name
     click_link ticket.title
+    click_link "Edit Ticket"
   end
   
-  scenario "Deleting a ticket" do
-    click_link "Delete Ticket"
-    page.should have_content("Ticket has been deleted.")
-    page.current_url.should == project_url(project)
+  scenario "Updating a ticket" do
+    fill_in "Title", :with => "Make it really shiny!"
+    click_button "Update Ticket"
+    page.should have_content "Ticket has been updated."
+    within("#ticket h2") do
+      page.should have_content("Make it really shiny!")
+    end
+    page.should_not have_content ticket.title
+  end
+  
+  scenario "Updating a ticket with invalid information" do
+    fill_in "Title", :with => ""
+    click_button "Update Ticket"
+    page.should have_content("Ticket has not been updated.")
   end
 end
